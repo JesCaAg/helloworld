fe35f2b1df783a9b9d04285b14029bb685dd29f5
JesCaAg <jesus.carral133@comunidadunir.net>
2025-05-08 11:00:43 +0200

    Anadidos test rest de multiply y divide en api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -33,6 +34,101 @@ class TestApi(unittest.TestCase):
         )
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
+        )
+
+    def test_api_divide_exact(self):
+        url = f"{BASE_URL}/calc/divide/6/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2.0", "ERROR DIVDE EXACT"
+        )
+
+    def test_api_divide_decimal(self):
+        url = f"{BASE_URL}/calc/divide/7/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "3.5", "ERROR DIVDE DECIMAL"
+        )
+
+    def test_api_divide_negative(self):
+        url = f"{BASE_URL}/calc/divide/10/-2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "-5.0", "ERROR DIVIDE NEGATIVE"
+        )
+
+    def test_api_divide_by_one(self):
+        url = f"{BASE_URL}/calc/divide/9/1"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "9.0", "ERROR DIVIDE BY ONE"
+        )
+
+    def test_api_divide_by_zero(self):
+        url = f"{BASE_URL}/calc/divide/5/0"
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.fail(
+                f"Se esperaba codigo HTTP 406 en {url}"
+            )
+        except HTTPError as e:
+            self.assertEqual(
+                e.code, 406, f"Se esperaba codigo 406, pero se recibio {e.code} en {url}"
+            )
+        
+    
+    def test_api_divide_char(self):
+        url = f"{BASE_URL}/calc/divide/z/3"
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.fail(
+                f"Se esperaba codigo HTTP 400 en {url}"
+            )
+        except HTTPError as e:
+            self.assertEqual(
+                e.code, 400, f"Se esperaba codigo 400, pero se recibio {e.code} en {url}"
+            )
+
+    def test_api_multiply_basic(self):
+        url = f"{BASE_URL}/calc/multiply/4/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "20", f"EROR MULTIPLY BASIC"
+        )
+
+    def test_api_multiply_negative(self):
+        url = f"{BASE_URL}/calc/multiply/-3/6"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "-18", f"ERROR MULTIPLY NEGATIVE"
+        )
+
+    def test_api_multiply_by_zero(self):
+        url = f"{BASE_URL}/calc/multiply/7/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "0", f"ERROR MULTIPLY BY ZERO"
         )
 
 if __name__ == "__main__":  # pragma: no cover
